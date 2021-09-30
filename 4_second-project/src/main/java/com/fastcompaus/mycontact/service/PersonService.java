5dd69f308934b04a1263a8aa56552e7f5e93d1d2
박빈나 <binna@bagbinnaui-MacBookPro.local>
2021-10-01 00:12:35 +0900

    JPA Relation

--- 4_second-project/src/main/java/com/fastcompaus/mycontact/service/PersonService.java
+++ 4_second-project/src/main/java/com/fastcompaus/mycontact/service/PersonService.java
@@ -4,12 +4,15 @@ import com.fastcompaus.mycontact.domain.Block;
 import com.fastcompaus.mycontact.domain.Person;
 import com.fastcompaus.mycontact.reporitory.BlockRepository;
 import com.fastcompaus.mycontact.reporitory.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class PersonService {
     @Autowired
@@ -33,5 +36,15 @@ public class PersonService {
                 .stream()
                 .filter(person -> person.getBlock() == null)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)     // 저장하는 쿼리는 없고 조회하는 쿼리만 있을때 사용 중요!
+    public Person getPerson(Long id) {
+        Person person = personRepository.findById(id).get();
+
+        System.out.println("person : " + person);
+        log.info("person : {}", person);
+
+        return person;
     }
 }
--- 4_second-project/src/test/java/com/fastcompaus/mycontact/service/PersonServiceTest.java
+++ 4_second-project/src/test/java/com/fastcompaus/mycontact/service/PersonServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -22,7 +23,7 @@ class PersonServiceTest {
     @Test
     void getPeopleExcludeBlocks() {
         givenPerson();
-        givenBlock();
+//        givenBlock();
 
         List<Person> result = personService.getPeopleExcludeBlocks();
 
@@ -30,13 +31,47 @@ class PersonServiceTest {
         result.forEach(System.out::println);
     }
 
-    private void givenBlock() {
-        givenBlock("martin");
+    @Test
+    void cascadeTest() {
+        givenPerson();
+
+        List<Person> result = personRepository.findAll();
+
+        result.forEach(System.out::println);
+
+
+        Person person = result.get(3);
+
+        person.getBlock().setStartDate(LocalDate.now());
+        person.getBlock().setEndDate(LocalDate.now());
+
+        personRepository.save(person);
+        personRepository.findAll().forEach(System.out::println);
+
+//        personRepository.delete(person);
+//        personRepository.findAll().forEach(System.out::println);
+//        blockRepository.findAll().forEach(System.out::println);
+
+        person.setBlock(null);
+        personRepository.save(person);
+        personRepository.findAll().forEach(System.out::println);
+        blockRepository.findAll().forEach(System.out::println);
     }
 
-    private Block givenBlock(String name) {
-        return blockRepository.save(new Block(name));
+    @Test
+    void getPerson() {
+        givenPerson();
+
+        Person person = personService.getPerson(3L);
+
+        System.out.println(person);
     }
+//    private void givenBlock() {
+//        givenBlock("martin");
+//    }
+//    private Block givenBlock(String name) {
+//        return blockRepository.save(new Block(name));
+//    }
 
     private void givenPerson() {
         givenPerson("martin", 10, "A");
@@ -51,7 +86,8 @@ class PersonServiceTest {
 
     private void givenBlockPerson(String name, int age, String bloodType) {
         Person blockPerson = new Person(name, age, bloodType);
-        blockPerson.setBlock(givenBlock(name));
+//        blockPerson.setBlock(givenBlock(name));
+        blockPerson.setBlock(new Block(name));
 
         personRepository.save(blockPerson);
     }
