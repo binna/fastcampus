39561b2381b38a9587b914ae331325a4342f6944
박빈나 <binna@bagbinnaui-MacBookPro.local>
2021-09-30 19:11:57 +0900

    lombok

--- 4_second-project/src/main/java/com/fastcompaus/mycontact/domain/Person.java
+++ 4_second-project/src/main/java/com/fastcompaus/mycontact/domain/Person.java
@@ -1,51 +1,180 @@
 package com.fastcompaus.mycontact.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
+//@Getter
+//@Setter
+//@ToString(exclude = "phoneNumber")
+//@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+//@RequiredArgsConstructor
+//@EqualsAndHashCode
+@Data       // @Getter + @Setter + @RequiredArgsConstructor + @ToString + @EqualsAndHashCode.
 public class Person {
+
     @Id
     @GeneratedValue
     private Long id;
 
+//    @Getter
+//    @Setter
+    @NonNull        // import lombok.NonNull
     private String name;
 
+//    @Getter
+//    @Setter
+    @NonNull        // import lombok.NonNull
     private int age;
 
+//    @Getter
+//    @Setter
+    private String hobby;
 
-    public Long getId() {
-        return id;
-    }
+//    @Getter
+//    @Setter
+    @NonNull
+    private String bloodType;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    @Getter
+//    @Setter
+    private String address;
 
-    public String getName() {
-        return name;
-    }
+//    @Getter
+//    @Setter
+    private LocalDate birthDate;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    @Getter
+//    @Setter
+    private String job;
 
-    public int getAge() {
-        return age;
-    }
+    @ToString.Exclude
+    private String phoneNumber;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getHobby() {
+//        return hobby;
+//    }
+//    public void setHobby(String hobby) {
+//        this.hobby = hobby;
+//    }
+//
+//    public String getBloodType() {
+//        return bloodType;
+//    }
+//    public void setBloodType(String bloodType) {
+//        this.bloodType = bloodType;
+//    }
+//
+//    public String getAddress() {
+//        return address;
+//    }
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public LocalDate getBirthDate() {
+//        return birthDate;
+//    }
+//    public void setBirthDate(LocalDate birthDate) {
+//        this.birthDate = birthDate;
+//    }
+//
+//    public String getJob() {
+//        return job;
+//    }
+//    public void setJob(String job) {
+//        this.job = job;
+//    }
 
+//    @Override
+//    public String toString() {
+//        return "Person{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+//    }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Person{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", age=" + age +
+//                ", hobby='" + hobby + '\'' +
+//                ", bloodType='" + bloodType + '\'' +
+//                ", address='" + address + '\'' +
+//                ", birthDate=" + birthDate +
+//                ", job='" + job + '\'' +
+//                '}';
+//    }
+
+//    public Person() {}
+//
+//    public Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public Person(Long id, String name, int age, String hobby, String bloodType,
+//                  String address, LocalDate birthDate, String job, String phoneNumber) {
+//        this.id = id;
+//        this.name = name;
+//        this.age = age;
+//        this.hobby = hobby;
+//        this.bloodType = bloodType;
+//        this.address = address;
+//        this.birthDate = birthDate;
+//        this.job = job;
+//        this.phoneNumber = phoneNumber;
+//    }
+
+//    public boolean equals(Object object) {
+//        if (object == null) {
+//            return false;
+//        }
+//
+//        Person person = (Person) object;
+//
+//        if(!person.getName().equals(this.getName())) {
+//            return false;
+//        }
+//
+//        if(person.getAge() != this.getAge()) {
+//            return false;
+//        }
+//
+//        return true;
+//    }
+
+//    public int hashCode() {
+//        return (name + age).hashCode();
+//    }
 }
--- 4_second-project/src/test/java/com/fastcompaus/mycontact/reporitory/PersonRepositoryTest.java
+++ 4_second-project/src/test/java/com/fastcompaus/mycontact/reporitory/PersonRepositoryTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PersonRepositoryTest {
@@ -21,15 +23,43 @@ class PersonRepositoryTest {
         Person person = new Person();
         person.setName("martin");
         person.setAge(10);
+        person.setBloodType("A");
 
         personRepository.save(person);
 
-//        System.out.println(personRepository.findAll());
+        System.out.println(personRepository.findAll());
 
         List<Person> people = personRepository.findAll();
         assertThat(people.size()).isEqualTo(1);
         assertThat(people.get(0).getName()).isEqualTo("martin");
         assertThat(people.get(0).getAge()).isEqualTo(10);
+        assertThat(people.get(0).getBloodType()).isEqualTo("A");
+    }
+
+//    @Test
+//    void allArgsConstructor() {
+//        Person person = new Person(1L, "martin", 10, "reading", "A",
+//                "분당", LocalDate.of(2019, 1, 1), "programmer", "010-1111-1111");
+//    }
+//    @Test
+//    void constructorTest() {
+//        Person person = new Person("martin", 10);
+//    }
+
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("martin", 10, "A");
+        Person person2 = new Person("martin", 10,"A");
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+
+        Map<Person, Integer> map = new HashMap<>();
+        map.put(person1, person1.getAge());
+
+        System.out.println(map);
+        System.out.println(map.get(person2));
     }
 
 }
